495500393d55617fa9c6f1c75fdd1d46f406bd3c
RamanaiahPirla <ramanaiah.237@gmail.com>
2022-10-09 20:42:58 +0530

    reformatted code

--- src/main/java/org/camundabpmn/Application.java
+++ src/main/java/org/camundabpmn/Application.java
@@ -7,11 +7,11 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Loan API", version = "1.0.0-SNAPSHOT",description = "Loan Processing Application By Camunda Process Engine"))
+@OpenAPIDefinition(info = @Info(title = "Loan API", version = "1.0.0-SNAPSHOT", description = "Loan Processing Application By Camunda Process Engine"))
 public class Application {
 
-  public static void main(String... args) {
-    SpringApplication.run(Application.class, args);
-  }
+	public static void main(String... args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
